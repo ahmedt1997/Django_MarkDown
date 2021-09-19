03322a79607e69b7deb30d209b4b79048be876f5
Ahmed Touzani <ahmedbbccaa@gmail.com>
2021-09-19 04:10:33 +0100

    just finishing the part of search and filter

--- btre/settings.py
+++ btre/settings.py
@@ -29,13 +29,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'realtors.apps.RealtorsConfig',
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '123456789',
+        'HOST':'localhost'
     }
 }
 
@@ -123,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- pages/views.py
+++ pages/views.py
@@ -4,10 +4,32 @@ from django.http import HttpResponse
 # Create your views here.
 from django.template.context_processors import request
 
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import price_choices,state_choices,bedroom_choices
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings,
+        'price_choices':price_choices,
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # get all the realtors
+    realtor = Realtor.objects.order_by('-hire_date')
+
+    #get MVP
+    mvp_realtor = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtor':realtor,
+        'mvp_realtor':mvp_realtor
+
+    }
+
+    return render(request, 'pages/about.html',context)
